57c67b7317d9712bfeb02cb1e86229de9186a0eb
lmmolinabe92 <lmmolinabe92@gmail.com>
2023-03-08 20:31:42 +0100

    terminado

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,21 @@ namespace Ejercicio9
          */
         static void Main(string[] args)
         {
+            
             string letras = "";
+            int intentos = 0;
             Console.WriteLine("Entre la palabra a adivinar");
             string palabra = Console.ReadLine();
-            bool fin;
+            DateTime inicio = DateTime.Now;
             do
             {
                 Console.Clear();
-                fin = mostrarPalabra(palabra, letras);
+                if (mostrarPalabra(palabra, letras))
+                {
+                    mostrarDibujo(intentos);
+                    break;
+                }
+                mostrarDibujo(intentos);
 
                 Console.WriteLine("Adivina una letra");
                 char letra = Console.ReadLine()[0];
@@ -30,8 +38,26 @@ namespace Ejercicio9
                 {
                     letras += letra;
                 }
-            } while (!fin);
-            Console.WriteLine("Has ganado.");
+                else
+                {
+                    intentos++;
+                    if (intentos == 6)
+                    {
+                        break;
+                    }
+                }
+            } while (true);
+            DateTime hfin = DateTime.Now;
+            TimeSpan tiempo = hfin - inicio;
+            if (intentos < 6)
+            {
+                Console.WriteLine("Has ganado.");
+            }
+            else
+            {
+                Console.WriteLine("Lo siento has perdido.");
+            }
+            Console.WriteLine("Has demorado " + tiempo.Minutes + ":" + tiempo.Seconds + " segundos.");
             Console.ReadLine();
         }
 
@@ -49,7 +75,93 @@ namespace Ejercicio9
                     fin = false;
                 }
             Console.WriteLine("\n");
+
             return fin;
+        }
+
+        public static void mostrarDibujo(int intentos)
+        {
+            switch (intentos)
+            {
+                case 0:
+                    Console.WriteLine(
+    "  +---+\n" +
+    "  |   |\n" +
+    "      |\n" +
+    "      |\n" +
+    "      |\n" +
+    "      |\n" +
+    "========="
+    );
+                    break;
+                case 1:
+                    Console.WriteLine(
+    "  +---+\n" +
+    "  |   |\n" +
+    "  0   |\n" +
+    "      |\n" +
+    "      |\n" +
+    "      |\n" +
+    "========="
+    );
+                    break;
+                case 2:
+                    Console.WriteLine(
+    "  +---+\n" +
+    "  |   |\n" +
+    "  0   |\n" +
+    "  |   |\n" +
+    "      |\n" +
+    "      |\n" +
+    "========="
+    );
+                    break;
+                case 3:
+                    Console.WriteLine(
+    "  +---+\n" +
+    "  |   |\n" +
+    "  0   |\n" +
+    " /|   |\n" +
+    "      |\n" +
+    "      |\n" +
+    "========="
+    );
+                    break;
+                case 4:
+                    Console.WriteLine(
+    "  +---+\n" +
+    "  |   |\n" +
+    "  0   |\n" +
+    " /|\\  |\n" +
+    "      |\n" +
+    "      |\n" +
+    "========="
+    );
+                    break;
+                case 5:
+                    Console.WriteLine(
+    "  +---+\n" +
+    "  |   |\n" +
+    "  0   |\n" +
+    " /|\\  |\n" +
+    " /    |\n" +
+    "      |\n" +
+    "========="
+    );
+                    break;
+                case 6:
+                    Console.WriteLine(
+    "  +---+\n" +
+    "  |   |\n" +
+    "  0   |\n" +
+    " /|\\  |\n" +
+    " / \\  |\n" +
+    "      |\n" +
+    "========="
+    );
+                    break;
+
+            }
         }
     }
 }
